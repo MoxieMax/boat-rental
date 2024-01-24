df29b3c6bafeed69d2cf21214e0a4cde1af5592d
Max MacGillivray <mary.macgillivray@gmail.com>
2024-01-24 13:34:08 -0700

    Created initial test outline for #rent method on Dock class.

--- lib/dock.rb
+++ lib/dock.rb
@@ -5,5 +5,10 @@ class Dock
   def initialize(name, time)
     @name = name
     @max_rental_time = time
+    @rental_log = {}
+  end
+  
+  def rent(boat, renter)
+    
   end
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -6,8 +6,13 @@ require './lib/dock'
 RSpec.describe Dock do
   # before :each do
     let (:dock1) {Dock.new("The Rowing Dock", 3)}
+    
     let (:rent1) {Renter.new("Patrick Star", "4242424242424242")}
+    let (:rent1) {Renter.new("Eugene Crabs", "1313131313131313")}
+    
     let (:boat1) {Boat.new(type: :kayak, price: 20)}
+    let (:boat2) {Boat.new(type: :kayak, price: 20)}
+    let (:boat3) {Boat.new(type: :standup_paddle_board, price: 15)}
   # end
   describe '#initialize' do
     it 'exists' do
@@ -17,6 +22,16 @@ RSpec.describe Dock do
     it 'has attributes' do
       expect(dock1.name).to eq("The Rowing Dock")
       expect(dock1.max_rental_time).to eq(3)
+    end
+  end
+  
+  describe '#rent' do
+    it 'assigns a boat to a renter' do
+      
+    end
+    
+    it 'updates the #rental_log' do
+      
     end
   end
 end
