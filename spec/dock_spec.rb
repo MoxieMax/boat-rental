b851612a8c2e23598cc9208fb110a8dd0ca5f581
Max MacGillivray <mary.macgillivray@gmail.com>
2024-01-24 14:05:46 -0700

    Created the #rent method for Dock class.

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -5,14 +5,14 @@ require './lib/dock'
 
 RSpec.describe Dock do
   # before :each do
-    let (:dock1) {Dock.new("The Rowing Dock", 3)}
-    
-    let (:rent1) {Renter.new("Patrick Star", "4242424242424242")}
-    let (:rent1) {Renter.new("Eugene Crabs", "1313131313131313")}
-    
     let (:boat1) {Boat.new(type: :kayak, price: 20)}
     let (:boat2) {Boat.new(type: :kayak, price: 20)}
     let (:boat3) {Boat.new(type: :standup_paddle_board, price: 15)}
+    
+    let (:rent1) {Renter.new("Patrick Star", "4242424242424242")}
+    let (:rent2) {Renter.new("Eugene Crabs", "1313131313131313")}
+    
+    let (:dock1) {Dock.new("The Rowing Dock", 3)}
   # end
   describe '#initialize' do
     it 'exists' do
@@ -26,12 +26,18 @@ RSpec.describe Dock do
   end
   
   describe '#rent' do
-    it 'assigns a boat to a renter' do
+    it 'assigns a boat to a renter and updates the #rental_log' do
+      expect(dock1.rental_log).to eq({})
       
-    end
-    
-    it 'updates the #rental_log' do
+      dock1.rent(boat1, rent1)
+      dock1.rent(boat2, rent1)
+      dock1.rent(boat3, rent2)
       
+      expect(dock1.rental_log).to eq({
+                                      boat1 => rent1,
+                                      boat2 => rent1,
+                                      boat3 => rent2
+                                    })
     end
   end
 end
